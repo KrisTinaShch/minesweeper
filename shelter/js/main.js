df8794124e6b0f3ace2e38144652aff2100bb3a6
Kristina Shcheglova <kris.shcheglova@bk.ru>
2023-04-04 23:43:54 +0300

    feat: create infinity carousel , 23:43, 04.04.23

--- shelter/js/main.js
+++ shelter/js/main.js
@@ -40,3 +40,45 @@ if (menuBurger) {
 
 
 // carousel
+
+// const carouselOptions = {
+//     infinityScroll: true,
+//     maxVisibleItems: 3,
+//     maxScrollItems: 3,
+// }
+
+const carousel = document.querySelector('.carousel');
+const container = carousel.querySelector('.carousel-container');
+const prevButton = carousel.querySelector('.carousel-left');
+const nextButton = carousel.querySelector('.carousel-right');
+const carouselItems = carousel.querySelectorAll('.carousel-item');
+const itemWidth = carouselItems[0].offsetWidth;
+
+let currentIndex = 1;
+let position = 0;
+let maxVisibleItems = 3;
+
+// добавляем копию первого слайда в конец списка слайдов
+container.appendChild(carouselItems[0].cloneNode(true));
+
+function initSlider() {
+
+    nextButton.addEventListener('click', () => {
+        currentIndex += maxVisibleItems;
+        position = position - itemWidth * maxVisibleItems;
+
+        // если достигнут последний слайд, переходим на первый слайд
+        if (currentIndex > carouselItems.length) {
+            currentIndex = 1;
+            position = 0;
+        }
+
+        container.style.transition = `transform 500ms ease-in-out`;
+        container.style.transform = `translateX(${position}px)`;
+    });
+}
+
+initSlider();
+// position -= itemWidth;
+// position = Math.max(position, -itemWidth * (items.length - maxItems));
+// container.style.transform = `translateX(${position}px)`;
