a810a404f6cd84bc9e7e34e07cfa6ae5b11c2906
Kris Shcheglova <kris.shcheglova@bk.ru>
2023-05-21 11:40:03 +0300

    feat: create flags

--- minesweeper/script.js
+++ minesweeper/script.js
@@ -1,12 +1,10 @@
-// Creating a normal board 10x10
 class NormalBoard {
-  constructor(width, height, bombCount, number = 0, bombCoords = [], cellsArray = []) {
+  constructor(width, height, bombCount) {
     this.width = width;
     this.height = height;
     this.bombCount = bombCount;
-    this.number = number;
-    this.bombCoords = bombCoords;
-    this.cellsArray = cellsArray;
+    this.bombCoords = [];
+    this.cellsArray = [];
   }
 
   generateBombs() {
@@ -20,27 +18,37 @@ class NormalBoard {
         bombs.push([row, col]);
       }
     }
-    console.log(bombs)
+
+    this.bombCoords = bombs;
+    console.log(bombs);
     return bombs;
   }
 
   updateCellsArray() {
     const cells = [];
-    const bombCoords = this.generateBombs();
-    this.bombCoords = bombCoords;
+
+    this.bombCoords = this.generateBombs();
 
     for (let i = 0; i < this.width; i += 1) {
-      cells.push([]);
+      const row = [];
       for (let j = 0; j < this.height; j += 1) {
-        const isBomb = bombCoords.some(([row, col]) => i === row && j === col);
-        cells[i].push(isBomb ? 'x' : ' ');
+        const isBomb = this.bombCoords.some(([row, col]) => i === row && j === col);
+        row.push(isBomb ? 'x' : ' ');
       }
+      cells.push(row);
     }
 
-    bombCoords.forEach(([i, j]) => {
+    this.bombCoords.forEach(([i, j]) => {
       for (let k = i - 1; k < i + 2; k += 1) {
         for (let l = j - 1; l < j + 2; l += 1) {
-          if (k >= 0 && l >= 0 && k < 10 && cells[k][l] !== 'x' && k < cells.length && l < cells[k].length) {
+          if (
+            k >= 0 &&
+            l >= 0 &&
+            k < 10 &&
+            cells[k][l] !== 'x' &&
+            k < cells.length &&
+            l < cells[k].length
+          ) {
             if (typeof cells[k][l] !== 'number') {
               cells[k][l] = 1;
             } else {
@@ -50,6 +58,7 @@ class NormalBoard {
         }
       }
     });
+
     this.cellsArray = cells;
     return cells;
   }
@@ -60,19 +69,24 @@ class NormalBoard {
 
     const mineField = document.createElement('div');
     mineField.classList.add('mine-field');
+
     const cells = this.updateCellsArray();
-    for (let i = 0; i < this.width; i += 1) {
+
+    cells.forEach((row, i) => {
       const fieldRow = document.createElement('div');
       fieldRow.classList.add('field-row');
-      for (let j = 0; j < this.height; j += 1) {
-        const fieldCells = document.createElement('div');
-        fieldCells.classList.add('field-cell');
-        fieldCells.id = `[${i}, ${j}]`;
-        fieldCells.innerHTML = cells[i][j];
-        fieldRow.appendChild(fieldCells);
-      }
+
+      row.forEach((cell, j) => {
+        const fieldCell = document.createElement('div');
+        fieldCell.classList.add('field-cell');
+        fieldCell.id = `[${i}, ${j}]`;
+        fieldCell.innerHTML = cell;
+        fieldRow.appendChild(fieldCell);
+      });
+
       mineField.appendChild(fieldRow);
-    }
+    });
+
     container.appendChild(mineField);
     document.body.appendChild(container);
 
@@ -81,17 +95,20 @@ class NormalBoard {
 
   generateCover() {
     let clicksCounter = 0;
-    const fieldCells = document.querySelectorAll('.field-cell');
     const mineField = document.querySelector('.mine-field');
     const gameOver = document.createElement('div');
-    let isGameOver = false;
-    Array.from(fieldCells).forEach(fieldCell => {
-      fieldCell.addEventListener('click', (event) => {
-        const [cellI, cellJ] = fieldCell.id
+    const fieldCells = document.querySelectorAll('.field-cell');
+    gameOver.classList.add('game-over-overlay');
+
+    mineField.addEventListener('click', (event) => {
+      const target = event.target;
+      if (target.classList.contains('field-cell')) {
+        const [cellI, cellJ] = target.id
           .replace('[', '')
           .replace(']', '')
           .split(',')
           .map(coord => parseInt(coord.trim(), 10));
+
         this.bombCoords.forEach(([i, j]) => {
           if (clicksCounter === 0) {
             if (i === cellI && j === cellJ) {
@@ -103,37 +120,31 @@ class NormalBoard {
             }
           } else {
             if (i === cellI && j === cellJ) {
-              fieldCell.classList.add('field-bomb');
-              gameOver.classList.add('game-over-overlay');
-              isGameOver = true;
-              fieldCells.forEach(cell => {
+              target.classList.add('field-bomb');
+              gameOver.innerHTML = '<p class="overlay-text">Game over</p>';
+              mineField.querySelectorAll('.field-cell').forEach(cell => {
                 cell.classList.add('open-cells');
               });
-              const youLooseModal = document.createElement('p');
-              youLooseModal.textContent = 'Game over';
-              youLooseModal.classList.add('overlay-text');
-              gameOver.appendChild(youLooseModal);
               mineField.appendChild(gameOver);
             }
           }
         });
-        this.cellsArray[cellI][cellJ] = 'checked';
-        // console.log(this.cellsArray);
 
-        // check win
+        this.cellsArray[cellI][cellJ] = 'checked';
+
         if (this.checkCells(this.cellsArray)) {
-          const youWinModal = document.createElement('p');
-          youWinModal.textContent = 'You win';
-          youWinModal.classList.add('overlay-text');
+          gameOver.innerHTML = '<p class="overlay-text">You win</p>';
           gameOver.classList.add('win-overlay');
-          gameOver.appendChild(youWinModal);
           mineField.appendChild(gameOver);
         }
-        // add numbers colors
-        this.generateCellsColor(event, fieldCell);
-        fieldCell.classList.add('open-cells');
-      });
+
+        console.log(this.cellsArray);
+        this.generateCellsColor(target);
+        target.classList.add('open-cells');
+      }
     });
+
+    this.makeFlagOnCell(fieldCells);
   }
 
   checkCells(cellsArray) {
@@ -147,25 +158,97 @@ class NormalBoard {
     return true;
   }
 
-  generateCellsColor(event, fieldCell) {
-    if (event.target.innerHTML === '1') {
-      fieldCell.classList.add('field-one');
-    } else if (event.target.innerHTML === '2') {
-      fieldCell.classList.add('field-two');
-    } else if (event.target.innerHTML === '3') {
-      fieldCell.classList.add('field-three');
-    } else if (event.target.innerHTML === '4') {
-      fieldCell.classList.add('field-four');
-    } else if (event.target.innerHTML === '5') {
-      fieldCell.classList.add('field-five');
-    } else if (event.target.innerHTML === ' ') {
-      fieldCell.classList.add('field-empty');
+  makeFlagOnCell(fieldCells, isFlagged = false) {
+    let flagArrayCoords = [];
+
+    fieldCells.forEach(fieldCell => {
+      fieldCell.addEventListener('contextmenu', event => {
+        event.preventDefault();
+        const target = event.target;
+
+        if (target.classList.contains('field-cell')) {
+          const [cellI, cellJ] = target.id
+            .replace('[', '')
+            .replace(']', '')
+            .split(',')
+            .map(coord => parseInt(coord.trim(), 10));
+
+          this.bombCoords.forEach(([i, j]) => {
+            if (i === cellI && j === cellJ) {
+              const flagIndex = flagArrayCoords.findIndex(coord => coord[0] === i && coord[1] === j);
+              if (!fieldCell.querySelector('.flag-img')) {
+                if (flagIndex === -1) {
+                  console.log('ok');
+                  flagArrayCoords.push([i, j]);
+                  console.log(flagArrayCoords);
+                }
+              } else {
+                if (flagIndex !== -1) {
+                  flagArrayCoords.splice(flagIndex, 1);
+                  console.log(flagArrayCoords);
+                }
+              }
+            }
+          });
+        }
+
+        if (!fieldCell.querySelector('.flag-img')) {
+          const flag = document.createElement('img');
+          flag.src = 'assets/imgs/icon_mine.png';
+          flag.classList.add('flag-img');
+          fieldCell.appendChild(flag);
+          isFlagged = true;
+        } else {
+          const flag = fieldCell.querySelector('.flag-img');
+          fieldCell.removeChild(flag);
+          isFlagged = false;
+        }
+
+        // Проверка на победу
+        const hasWon = this.checkVictory(flagArrayCoords, this.bombCoords);
+        if (hasWon) {
+          console.log('Поздравляем! Вы выиграли!');
+        }
+      });
+    });
+
+    return isFlagged;
+  }
+
+  checkVictory(flagArrayCoords, bombCoords) {
+    if (flagArrayCoords.length !== bombCoords.length) {
+      return false;
+    }
+    return flagArrayCoords.every(coord => bombCoords.some(([i, j]) => i === coord[0] && j === coord[1]));
+  }
+
+
+  generateCellsColor(target) {
+    const cellValue = target.innerHTML;
+    switch (cellValue) {
+      case '1':
+        target.classList.add('field-one');
+        break;
+      case '2':
+        target.classList.add('field-two');
+        break;
+      case '3':
+        target.classList.add('field-three');
+        break;
+      case '4':
+        target.classList.add('field-four');
+        break;
+      case '5':
+        target.classList.add('field-five');
+        break;
+      case ' ':
+        target.classList.add('field-empty');
+        break;
+      default:
+        break;
     }
   }
 }
 
 const normalBoard = new NormalBoard(10, 10, 10);
-
-// normalBoard.generateCellsCover();
-
 normalBoard.generateCellsCover();
